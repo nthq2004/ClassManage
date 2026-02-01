21325c3c87c6e0c1c95e5b2db0b8fbca0c0b1a8d
nthq2004 <ldcrwaj@sina.com>
2026-02-01 19:00:03 +0800

    增加了直流电源

--- simulation.js
+++ simulation.js
@@ -1,4 +1,6 @@
 import { Gauge } from './guage.js';
+import { DCPower } from './DCPowerSupply.js';
+
 /*对外声明的类，构造时要传入画布ID，和处理函数，所有的仿真对象都包含在这个文件 */
 export class SimulationEngine {
     constructor(containerId, onAction) {
@@ -23,11 +25,6 @@ export class SimulationEngine {
     }
 
     init() {
-        // 创建船舶柴油机冷却水系统组件
-        /*         this.createComp('Diesel', 50, 80, '#e67e22', '柴油机');
-                // 功能控制组
-                this.createActionBtn('Pipe', 220, 80, '#27ae60', '自动连管'); */
-
 
         const aGauge = new Gauge({
             layer: this.layer,
@@ -43,7 +40,7 @@ export class SimulationEngine {
             onTerminalClick: this.onTermClick.bind(this)
         })
 
-         const pGauge = new Gauge({
+        const pGauge = new Gauge({
             layer: this.layer,
             id: 'gaugePressure',
             name: '压力表bar',
@@ -55,7 +52,17 @@ export class SimulationEngine {
             radius: 80,
             type: 'pGauge',
             onTerminalClick: this.onTermClick.bind(this)
-        })       
+        })
+
+        const myPower = new DCPower({
+            layer: this.layer,
+            id: 'dcPower1',
+            name: '直流电源V',
+            x: 50,
+            y: 50,
+            voltage: 12,
+            onTerminalClick: this.onTermClick.bind(this)
+        })
 
         this.layer.draw();
     }
@@ -96,12 +103,7 @@ export class SimulationEngine {
     }
     /*每一个功能设备都是一个group，典型包括外壳、小组件、文字等，name属性用.查找，代表一类设备或一类属性，id属性用#查找，代表独一无二节点 */
     /*     createComp(id, x, y, color, label) {
-            const group = new Konva.Group({ x, y, id, name: 'device' ,draggable: true });
-            group.add(new Konva.Rect({ width: 150, height: 90, fill: color, stroke: '#fff', cornerRadius: 5 }));
-            /*listening: false,不参与点击事件，Canvas不会进行命中检测 
-            group.add(new Konva.Text({ text: label, x: 10, y: 35, fill: '#fff', fontStyle: 'bold', listening: false }));
             const light = new Konva.Circle({ x: 135, y: 15, radius: 6, fill: 'red', name: 'status' });
-            group.add(light);
             //为组件定义点击处理函数，首先功能设备主要是启停，设置work属性，OFF表示停止，ON表示正在工作，点击后状态取反，存入newState,调用状态更新函数和用户定义的函数。状态更新函数是改变设备本身的显示状态。
             // 使用自定义点击检测
             group.on('click tap', (e) => {
@@ -112,22 +114,17 @@ export class SimulationEngine {
                 this.updateState(id, newState);
                 this.onAction(id, newState);
             });
-            this.layer.add(group);
             //有了以下指令，根据仿真系统对象，可根据id访问所有的设备group 
             this.devices[id] = group;
         } */
 
     /*     createActionBtn(id, x, y, color, label) {
             //这些是功能按键，不是设备，没有device的name属性 
-            const btn = new Konva.Group({ x, y, id ,draggable: true });
-            btn.add(new Konva.Rect({ width: 130, height: 40, fill: color, cornerRadius: 20 }));
             btn.add(new Konva.Text({ text: label, x: 35, y: 14, fill: '#fff', listening: false }));
             btn.on('click tap', () => {
                 if (this.isLocked) return;
                 //告诉main.js这个功能按键被点击了，完全交给外面处理 
                 this.onAction(id, 'TRIGGER');
-            });
-            this.layer.add(btn);
         } */
 
     updateState(id, state) {
@@ -140,10 +137,4 @@ export class SimulationEngine {
         if (light) light.fill(state === 'ON' ? '#2ecc71' : 'red');
         this.layer.batchDraw();
     }
-
-    /*     fit() {
-            const container = this.container;
-            this.stage.width(container.offsetWidth);
-            this.stage.height(container.offsetHeight);
-        } */
 }
